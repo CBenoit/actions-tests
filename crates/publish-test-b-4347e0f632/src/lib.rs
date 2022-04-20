1f99f44bae05068e51c7aa269cea1f34a2f55999
Benoît CORTIER <bcortier@pm.me>
2022-04-20 17:19:14 -0400

    feat(publish-test-b-4347e0f632): add

--- crates/publish-test-b-4347e0f632/src/lib.rs
+++ crates/publish-test-b-4347e0f632/src/lib.rs
@@ -1,8 +1,14 @@
+pub fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
+        let result = add(2, 2);
         assert_eq!(result, 4);
     }
 }
